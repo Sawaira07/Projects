0f90ed54b124e50ba34a082191879a11b9de755e
Zahra Batool <bzahra165@gmail.com>
2023-08-27 19:26:08 +0000

    Main.js edited online with Bitbucket

--- src/Main.js
+++ src/Main.js
@@ -17,12 +17,12 @@ const Main = (props) => {
       />
     </View>
     <View style={{ marginHorizontal: 5, marginVertical: 100 }}>
-    <Btn bgColor={darkGreen} textColor='white' btnLabel="Turn On Detection" Press={() => props.navigation.navigate("Login")} />
+    <Btn bgColor={darkGreen} textColor='white' btnLabel="Turn On Detection" Press={() => props.navigation.navigate("detector")} />
     <Btn bgColor={darkGreen} textColor= 'white' btnLabel="Tracker" Press={() => props.navigation.navigate("Signup")} />
     <Btn bgColor={darkGreen} textColor= 'white' btnLabel="FirstAid Booklet" Press={() => props.navigation.navigate("Guidebook")} />
     <Btn bgColor={darkGreen} textColor= 'white' btnLabel="How it Works?" Press={() => props.navigation.navigate("khan")} />
     <Btn bgColor={darkGreen} textColor= 'white' btnLabel="AboutUs" Press={() => props.navigation.navigate("Aboutus")} />
-    <Btn bgColor={darkGreen} textColor= 'white' btnLabel="Emergency" Press={() => props.navigation.navigate("khan")} />
+    <Btn bgColor={darkGreen} textColor= 'white' btnLabel="Emergency" Press={() => props.navigation.navigate("Numbers")} />
     </View>
    
                 
